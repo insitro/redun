d9eda2dc44eedaaa52616651b6b5fc5489c38659
Matt Rasmussen <matt.rasmus@gmail.com>
2021-10-09 14:15:30 -0700

    use public docker images

--- examples/setup_scheduler/workflow_lib/executors.py
+++ examples/setup_scheduler/workflow_lib/executors.py
@@ -12,7 +12,7 @@ class Account1Executor(AWSBatchExecutor):
     def __init__(self, name: str):
         config = Config({
             "executors.account1_executor": {
-                "image": f"YOUR_ACCOUNT1_ID.dkr.ecr.us-west-2.amazonaws.com/redun_aws_batch_example",
+                "image": f"YOUR_ACCOUNT1_ID.dkr.ecr.us-west-2.amazonaws.com/redun_example",
                 "queue": "YOUR_QUEUE_NAME1",
                 "s3_scratch": "s3://EXAMPLE_BUCKET1/redun/",
                 "job_name_prefix": "redun-example",
@@ -31,7 +31,7 @@ class Account2Executor(AWSBatchExecutor):
     def __init__(self, name: str):
         config = Config({
             "executors.account2_executor": {
-                "image": f"YOUR_ACCOUNT2_ID.dkr.ecr.us-west-2.amazonaws.com/redun_aws_batch_example",
+                "image": f"YOUR_ACCOUNT2_ID.dkr.ecr.us-west-2.amazonaws.com/redun_example",
                 "queue": "YOUR_QUEUE_NAME2",
                 "s3_scratch": "s3://EXAMPLE_BUCKET2/redun/",
                 "job_name_prefix": "redun-example",
