9d5f89e5ca1a13283c4bb22960742a2f5c5e2fef
Matt Rasmussen <matt.rasmus@gmail.com>
2021-10-13 12:45:40 -0700

    Make 0.8.0 release

--- docs/release_notes.py
+++ docs/release_notes.py
@@ -26,10 +26,10 @@ def main():
     print(f":Date: {today}\n")
 
     merge_base = subprocess.check_output(
-        f"git merge-base {args.current_release_tag} origin/master", shell=True
+        f"git merge-base {args.current_release_tag} origin/main", shell=True
     ).decode("utf8")
     commits = subprocess.check_output(
-        f"git --no-pager log --pretty='%s' --abbrev-commit {merge_base.strip()}..origin/master",
+        f"git --no-pager log --pretty='%s' --abbrev-commit {merge_base.strip()}..origin/main",
         shell=True,
     ).decode("utf8")
     for commit in commits.splitlines():
