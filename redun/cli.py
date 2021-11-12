e6d272977c54ebb54a5b7cf074e3e49ae85dc5bb
David Konerding <konerding.david@gene.com>
2021-11-12 18:09:50 +0000

    Print the name of the unimportable module using its filename.

--- redun/cli.py
+++ redun/cli.py
@@ -719,7 +719,11 @@ def import_script(filename_or_module: str, add_cwd: bool = True) -> ModuleType:
             add_import_path(os.getcwd())
         module_name = filename_or_module
 
-    module = importlib.import_module(module_name)
+    try:
+        module = importlib.import_module(module_name)
+    except ModuleNotFoundError as e:
+        logger.error(f"Failed to run workflow from Python script file named {filename_or_module}.")
+        raise
     return module
 
 
