8ff094fe393d3581435f96789ec16af8c8a521b3
David Konerding <konerding.david@gene.com>
2022-02-02 18:03:33 +0000

    more test jobs for k8s

--- examples/k8s/workflow.py
+++ examples/k8s/workflow.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import time
 from typing import Dict
+import random
 
 from redun import File, script, task
 
@@ -10,11 +11,48 @@ from redun import File, script, task
 redun_namespace = "redun.examples.k8s"
    
 
+
+@task(script=True, executor="k8s")
+def script_false_on_k8s():
+    return "false"
+
+
+@task(script=True, executor="k8s")
+def script_true_on_k8s():
+    return "true"
+
+
+@task(executor="k8s")
+def subtask_sysexit1(x):
+    sys.exit(1)
+
+
+@task(executor="k8s")
+def subtask_sysexit0(x):
+    sys.exit(0)
+
+@task(executor="k8s")
+def subtask_random_sleep(x):
+    t = random.uniform(10,60)
+    time.sleep(t)
+    coin = random.getrandbits(1)
+    print(coin)
+    return coin
+
 @task(executor="k8s")
 def subtask(x):
     time.sleep(100)
-    return x
+    coin = random.getrandbits(1)
+    print(coin)
+    return coin
     
+@task(executor="batch")
+def batch_subtask(x):
+    time.sleep(100000)
+    coin = random.getrandbits(1)
+    print(coin)
+    return coin
+
 @task(script=True, executor="k8s")
 def sleepy_script_on_k8s():
     return "sleep 60"
@@ -42,8 +80,11 @@ def failed_task_on_k8s() -> list:
 
 
 @task
-def main(n=10):
+def main(n: int=10):
+    #return [subtask_random_sleep(i) for i in range(n)]
     return [subtask(i) for i in range(n)]
+    #return [script_fail_on_k8s() for i in range(n)]
+    #return [batch_subtask(i) for i in range(n)]
 
 #     return [
 #         'main',
