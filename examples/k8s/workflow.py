fd5c6bbb9b96b27144dd7cd1f4d843ec270a8af5
David Konerding <konerding.david@gene.com>
2022-02-14 18:54:19 +0000

    clean up workflow a bit.

--- examples/k8s/workflow.py
+++ examples/k8s/workflow.py
@@ -1,104 +1,95 @@
-import sys
-import os
-import subprocess
 import time
-from typing import Dict
+import sys
 import random
-
 from redun import File, script, task
 
 
-redun_namespace = "redun.examples.k8s"
-   
+@task(executor="k8s")
+def waste_cpu():
+    """Script that wastes cpu (to test container CPU limits)"""
+    for j in range(100):
+        for i in range(10000000):
+            a = 5
+            a += 1
 
 
 @task(script=True, executor="k8s")
-def script_false_on_k8s(i):
+def script_false():
+    """Script that runs the false command"""
     return "false"
 
 
 @task(script=True, executor="k8s")
-def script_true_on_k8s(i):
+def script_true():
+    """Script that runs the true command"""
     return "true"
 
 
 @task(executor="k8s")
-def subtask_sysexit1(x):
+def sysexit1():
+    """Task that calls sys.exit(1).  This aborts redun oneshot inside the container, causing the job to fail with no useful logging info."""
     sys.exit(1)
 
 
 @task(executor="k8s")
-def subtask_sysexit0(x):
+def sysexit0():
+    """Task that calls sys.exit(0).  This aborts redun oneshot inside the container, causing the job to fail with no useful logging info"""
     sys.exit(0)
 
-@task(executor="k8s")
-def subtask_random_sleep(x):
-    t = random.uniform(1,2)
-    time.sleep(t)
-    coin = random.getrandbits(1)
-    print(coin)
-    return coin
 
 @task(executor="k8s")
-def subtask(x):
-    time.sleep(10)
-    coin = random.getrandbits(1)
-    print(coin)
-    return coin
-    
-@task(executor="batch")
-def batch_subtask(x):
-    time.sleep(100000)
-    coin = random.getrandbits(1)
-    print(coin)
-    return coin
+def task_raises_exception() -> list:
+    """Test task that raises exception.  This aborts redun oneshot inside the container, causing the job to fail with no useful logging info."""
+    raise RuntimeError
+
+
+@task(executor="k8s")
+def task_sleep():
+    """Task that sleeps for 60 seconds"""
+    time.sleep(60)
+
 
 @task(script=True, executor="k8s")
-def sleepy_script_on_k8s():
+def script_sleep():
+    """Script that sleeps for 60 seconds"""
     return "sleep 60"
 
-@task(executor='k8s')
-def task_on_k8s() -> list:
+
+@task(executor="k8s")
+def task_io() -> list:
+    """Task that writes to stdout and stderr"""
     return [
-        'task_on_k8s',
+        "task",
         print("hello stdout"),
         print("hello stderr", file=sys.stderr),
     ]
 
+
 @task()
-def script_on_k8s() -> File:
-   # The outer task is just for preparing the script and its arguments.
+def script_inside_task() -> File:
+    """Test script embedded in task"""
+    # The outer task is just for preparing the script and its arguments.
     return script(  # This inner task will run as a bash script inside the container.
-        f"ls /", 
+        f"ls /",
         executor="k8s",
         outputs=File("-"),
     )
 
-@task(executor='k8s')
-def failed_task_on_k8s() -> list:
-    raise RuntimeError
-
-
-
-@task()
-def script_on_k8s() -> File:
-   # The outer task is just for preparing the script and its arguments.
-    return script(  # This inner task will run as a bash script inside the container.
-        f"false", 
-    )
-
 
 @task
-def main(n: int=10):
-    return [subtask_random_sleep(i) for i in range(n)]
-    #return [subtask(i) for i in range(n)]
-    #return [script_true_on_k8s(i) for i in range(n)]
-    #return [batch_subtask(i) for i in range(n)]
-
-#     return [
-#         'main',
-#         sleepy_script_on_k8s(),
-#         #task_on_k8s(),
-#         #script_on_k8s(),
-#         #failed_task_on_k8s(),
-#     ]
+def main(n: int = 10):
+    """Main entrypoint for all tasks and scripts"""
+    return [
+        "main",
+        waste_cpu(),
+        script_true(),
+        task_sleep(),
+        script_sleep(),
+        task_io(),
+        script_inside_task(),
+        # The remainder of tasks abort redun oneshot inside the container.
+        # script_false(),
+        # sysexit1(),
+        # sysexit0(),
+        # task_raises_exception(),
+    ]
