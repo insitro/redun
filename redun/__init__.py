97814869593e6f28b0a39a5dee80d88195f23030
naegelyd-insitro <66270360+naegelyd-insitro@users.noreply.github.com>
2022-04-26 14:00:34 -0400

    Ignore flake8 F401 errors in __init__.py files (#136)

--- redun/__init__.py
+++ redun/__init__.py
@@ -1,10 +1,10 @@
-from redun.executors.aws_batch import AWSBatchExecutor  # noqa: F401
-from redun.executors.aws_glue import AWSGlueExecutor  # noqa: F401
-from redun.executors.local import LocalExecutor  # noqa: F401
-from redun.file import Dir, File, ShardedS3Dataset  # noqa: F401
-from redun.handle import Handle  # noqa: F401
-from redun.namespace import get_current_namespace, namespace  # noqa: F401
-from redun.scheduler import (  # noqa: F401
+from redun.executors.aws_batch import AWSBatchExecutor
+from redun.executors.aws_glue import AWSGlueExecutor
+from redun.executors.local import LocalExecutor
+from redun.file import Dir, File, ShardedS3Dataset
+from redun.handle import Handle
+from redun.namespace import get_current_namespace, namespace
+from redun.scheduler import (
     Scheduler,
     apply_tags,
     catch,
@@ -14,7 +14,7 @@ from redun.scheduler import (  # noqa: F401
     set_current_scheduler,
     throw,
 )
-from redun.scripting import script  # noqa: F401
-from redun.task import PartialTask, Task, get_task_registry, task  # noqa: F401
+from redun.scripting import script
+from redun.task import PartialTask, Task, get_task_registry, task
 
 __version__ = "0.8.8"
