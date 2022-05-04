b03f5794dd6639fb7b8219711468067853ee5639
Rico Meinl <rmeinl97@gmail.com>
2022-05-04 11:31:35 +0100

    Fixed typos and minor grammatical stuff like missing commas, etc. in the documentation and README.md

--- examples/02_compile/lib 2.c
+++
@@ -1,3 +0,0 @@
-char *get_message() {
-    return "Hello, World!\n";
-}
--- examples/02_compile/prog2 2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-char *get_message();
-
-int main(int argc, char** argv) {
-    char *msg = get_message();
-    printf("prog2: %s", msg);
-}
