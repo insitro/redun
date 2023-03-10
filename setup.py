1dad294d4c4104660dbfcb23bff991f02ad10877
ricomnl <rmeinl97@gmail.com>
2023-03-09 22:04:30 -0800

    Remove cloud-logging, not needed

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ requirements = [
     "sqlalchemy>=1.3.17,<2",
     "python-dateutil>=2.8",
     "google-cloud-batch>=0.2.0",
-    "google-cloud-logging",
     # If updating this list, check executors/aws_glue.py stays up to date with
     # packages needed to run in the glue environment.
 ]
