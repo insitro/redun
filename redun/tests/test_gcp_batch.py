6adfd11f8b513bec1ca46c194ec693694802c945
Matt Rasmussen <rasmus@insitro.com>
2023-04-30 16:05:22 -0700

    add gcp script task test

--- redun/tests/test_gcp_batch.py
+++ redun/tests/test_gcp_batch.py
@@ -9,6 +9,7 @@ from redun.config import Config
 from redun.executors.gcp_batch import GCPBatchExecutor
 from redun.expression import TaskExpression
 from redun.scheduler import Job, Traceback
+from redun.scripting import script, script_task
 from redun.tests.utils import (
     get_filesystem_class_mock,
     mock_s3,
@@ -306,3 +307,112 @@ def test_executor_array(
     # Ensure job returns results and errors to scheduler.
     assert scheduler.job_results[job1.id] == 10
     assert repr(scheduler.job_errors[job2.id]) == "ValueError('Boom')"
+
+
+@use_tempdir
+@mock_s3
+@patch("redun.executors.gcp_utils.get_gcp_client", Mock())
+@patch("redun.file.get_filesystem_class", get_filesystem_class_mock)
+@patch("redun.executors.gcp_utils.batch_submit")
+@patch("redun.executors.gcp_utils.list_jobs")
+@patch("redun.executors.gcp_utils.get_task")
+def test_executor_script(
+    get_task_mock: Mock,
+    list_jobs_mock: Mock,
+    batch_submit_mock: Mock,
+) -> None:
+    """
+    GCPBatchExecutor should run script jobs.
+    """
+    scheduler = mock_scheduler()
+    executor = mock_executor(scheduler)
+
+    # Prepare API mocks for job submission.
+    batch_job_id = "123"
+    batch_job = batch_v1.Job(task_groups=[batch_v1.TaskGroup(name=batch_job_id)])
+    batch_submit_mock.return_value = batch_job
+    list_jobs_mock.return_value = []
+    get_task_mock.return_value = batch_v1.Task(
+        name=f"{batch_job_id}/tasks/0",
+        status=batch_v1.TaskStatus(state=batch_v1.TaskStatus.State.SUCCEEDED),
+    )
+
+    # Create and submit a script job.
+    # Simulate the call to script_task().
+    expr = script(
+        "cat input > output",
+        inputs=[File("gs://example-bucket/input").stage("input")],
+        output=File("gs://example-bucket/output").stage("output"),
+    )
+    command = expr.args[0]
+    expr = script_task(command)
+    job = Job(script_task, expr)
+    job.eval_hash = "eval_hash"
+    job.args = (expr.args, expr.kwargs)
+    executor.submit_script(job)
+
+    # Let job arrayer submit job.
+    wait_until(lambda: executor.arrayer.num_pending == 0)
+
+    # Ensure job options were passed correctly to docker.
+    scratch_dir = executor.gcs_scratch_prefix
+    assert executor.code_file
+
+    assert batch_submit_mock.call_args
+    args = batch_submit_mock.call_args[1]
+    args.pop("client")
+    assert batch_submit_mock.call_args[1] == {
+        "commands": ["bash", "/mnt/disks/example-bucket/redun/jobs/eval_hash/.redun_job.sh"],
+        "image": "gcr.io/gcp-project/redun",
+        "job_name": f"redun-{job.id}",
+        "labels": {"redun_hash": "eval_hash", "redun_job_type": "script"},
+        "machine_type": "e2-standard-4",
+        "memory": 16,
+        "mount_buckets": ["example-bucket"],
+        "priority": 30,
+        "project": "project",
+        "region": "project",
+        "retries": 2,
+        "service_account_email": "",
+        "task_count": 1,
+        "vcpus": 2,
+    }
+
+    # Simulate output file created by job.
+    output_file = File(f"{scratch_dir}/jobs/eval_hash/output")
+    output_file.write("done")
+
+    # Manually run monitor logic.
+    executor._monitor()
+
+    # Ensure job returns result to scheduler.
+    assert scheduler.job_results[job.id] == [0, b"done"]
+
+    # Prepare API mocks for job submission.
+    batch_job_id = "456"
+    batch_job = batch_v1.Job(task_groups=[batch_v1.TaskGroup(name=batch_job_id)])
+    batch_submit_mock.return_value = batch_job
+    list_jobs_mock.return_value = []
+    get_task_mock.return_value = batch_v1.Task(
+        name=f"{batch_job_id}/tasks/0",
+        status=batch_v1.TaskStatus(state=batch_v1.TaskStatus.State.FAILED),
+    )
+
+    # Submit a failing script task.
+    job2 = Job(script_task, expr)
+    job2.eval_hash = "eval_hash2"
+    job2.args = (expr.args, expr.kwargs)
+    executor.submit_script(job2)
+
+    # Let job arrayer submit job.
+    wait_until(lambda: executor.arrayer.num_pending == 0)
+
+    # Simulate output file created by job.
+    error_file = File(f"{scratch_dir}/jobs/eval_hash2/error")
+    error_file.write("boom")
+
+    # Manually run monitor logic.
+    executor._monitor()
+
+    # Ensure job returns result to scheduler.
+    assert repr(scheduler.job_errors[job2.id]) == "ScriptError('Last line: boom')"
