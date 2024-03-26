3b0a9d28b19ca01ff5c3672dada3a96df0482600
magda-insitro <97103757+magda-insitro@users.noreply.github.com>
2024-03-26 12:27:14 +0100

    Refactor get_az_credential and cover more auth scenarios (#351)
    * Refactor get_az_credential and cover more auth scenarios
    
    * call get_token

--- redun/azure_utils.py
+++ redun/azure_utils.py
@@ -3,13 +3,36 @@ Helper functions for Azure integration.
 """
 import os
 
+import requests
+
 from redun.logging import logger
 
 try:
     from azure.ai.ml.identity import AzureMLOnBehalfOfCredential
-    from azure.identity import DefaultAzureCredential, ManagedIdentityCredential
+    from azure.identity import DefaultAzureCredential
 except (ImportError, ModuleNotFoundError):
     pass
+
+
+def is_azure_managed_node() -> bool:
+    """
+    Function that checks if the current compute is backed by an Azure VM.
+    Returns False for local devices or VMs hosted on other clouds (EC2).
+    https://learn.microsoft.com/en-us/azure/virtual-machines/instance-metadata-service?tabs=linux
+    """
+    imds_url = "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
+    headers = {"Metadata": "true"}
+
+    try:
+        response = requests.get(imds_url, headers=headers, timeout=2)
+        response.raise_for_status()
+        # If the request is successful, we are likely on an Azure VM
+        logger.debug("This node is an Azure-managed node.")
+        return True
+    except requests.RequestException:
+        # If the request fails, we are likely not on an Azure VM
+        logger.debug("This node is not an Azure-managed node.")
+        return False
 
 
 def get_az_credential():
@@ -29,29 +52,31 @@ def get_az_credential():
        assigned identity and we want to override it.
     """
 
-    try:
-        cr = DefaultAzureCredential()
-        # quick validation
-        cr.get_token("https://storage.azure.com/")
-        return cr
-    except:  # noqa: E722
-        # special credential available only inside AML compute
-        # when running with "user_identity"
-        # https://learn.microsoft.com/en-us/samples/azure/azureml-examples/azureml---on-behalf-of-feature/
-        if "AZUREML_WORKSPACE_ID" in os.environ:
-            try:
-                logger.info(
-                    "Failed to create DefaultAzureCredential, trying AzureMLOnBehalfOfCredential."
-                )
-                cr = AzureMLOnBehalfOfCredential()
-                cr.get_token("https://storage.azure.com/")
-                return cr
-            except:  # noqa: E722
-                pass
-        if "DEFAULT_IDENTITY_CLIENT_ID" in os.environ:
-            logger.info("Trying ManagedIdentityCredential with DEFAULT_IDENTITY_CLIENT_ID.")
-            cr = ManagedIdentityCredential(client_id=os.environ["DEFAULT_IDENTITY_CLIENT_ID"])
-            cr.get_token("https://storage.azure.com/")
-            return cr
+    if is_azure_managed_node():
+        mi_client_id = (
+            # the default name of the env variable, used by most tools
+            os.environ.get("AZURE_CLIENT_ID", None)
+            or
+            # passed to AML job via ManagedIdentityConfiguration
+            os.environ.get("AZUREML_USER_MANAGED_CLIENT_ID", None)
+            or
+            # available in AML clusters with user-assigned MI
+            os.environ.get("DEFAULT_IDENTITY_CLIENT_ID", None)
+        )
 
-    raise ValueError("Failed to create Azure credential.")
+        # try AzureMLOnBehalfOfCredential - it doesn't use exception classes
+        # from azure.identity package, so it's not compatible with ChainedTokenCredentials
+        # and we need to check beforehand if it's able to get any tokens
+        cr = AzureMLOnBehalfOfCredential()
+        try:
+            cr.get_token("https://storage.azure.com")
+        except:  # noqa: E722
+            cr = DefaultAzureCredential(managed_identity_client_id=mi_client_id)
+    else:
+        # don't attempt MI or AML credentials outside Azure
+        cr = DefaultAzureCredential(exclude_managed_identity_credential=True)
+
+    # validate if the credential can get tokens and fail early if not
+    cr.get_token("https://storage.azure.com")
+
+    return cr
--- redun/tests/test_azure_utils.py
+++ redun/tests/test_azure_utils.py
@@ -6,108 +6,107 @@ import pytest
 from redun import azure_utils
 
 
-@patch("redun.azure_utils.ManagedIdentityCredential")
 @patch("redun.azure_utils.AzureMLOnBehalfOfCredential")
 @patch("redun.azure_utils.DefaultAzureCredential")
-def test_azure_default_credential(
-    default_credential_mock, on_behalf_credential_mock, mi_credential_mock
+@patch("redun.azure_utils.is_azure_managed_node", return_value=False)
+def test_azure_default_credential_outside_azure(
+    _, default_credential_mock, on_behalf_credential_mock
 ):
     cr = azure_utils.get_az_credential()
+    default_credential_mock.assert_called_with(exclude_managed_identity_credential=True)
     assert cr == default_credential_mock.return_value
     on_behalf_credential_mock.assert_not_called()
-    mi_credential_mock.assert_not_called()
 
 
-@patch("redun.azure_utils.ManagedIdentityCredential")
 @patch("redun.azure_utils.AzureMLOnBehalfOfCredential")
 @patch("redun.azure_utils.DefaultAzureCredential")
-def test_azure_aml_fallback_credential(
-    default_credential_mock, on_behalf_credential_mock, mi_credential_mock
-):
-    os.environ["AZUREML_WORKSPACE_ID"] = "test"
-    default_credential_mock.return_value.get_token = MagicMock(
-        side_effect=Exception("failed to get token")
-    )
+@patch("redun.azure_utils.is_azure_managed_node", return_value=True)
+def test_azure_aml_credential_inside_azure(_, default_credential_mock, on_behalf_credential_mock):
     cr = azure_utils.get_az_credential()
-    del os.environ["AZUREML_WORKSPACE_ID"]
     assert cr == on_behalf_credential_mock.return_value
-
-    default_credential_mock.assert_called_once()
-    mi_credential_mock.assert_not_called()
+    default_credential_mock.assert_not_called()
 
 
-@patch("redun.azure_utils.ManagedIdentityCredential")
 @patch("redun.azure_utils.AzureMLOnBehalfOfCredential")
 @patch("redun.azure_utils.DefaultAzureCredential")
-def test_azure_default_identity_fallback_credential(
-    default_credential_mock, on_behalf_credential_mock, mi_credential_mock
+@patch("redun.azure_utils.is_azure_managed_node", return_value=True)
+def test_azure_default_credential_inside_azure(
+    _, default_credential_mock, on_behalf_credential_mock
 ):
-    os.environ["AZUREML_WORKSPACE_ID"] = "test"
-    os.environ["DEFAULT_IDENTITY_CLIENT_ID"] = "123-456-789"
-    default_credential_mock.return_value.get_token = MagicMock(
+    on_behalf_credential_mock.return_value.get_token = MagicMock(
         side_effect=Exception("failed to get token")
     )
+
+    cr = azure_utils.get_az_credential()
+    assert cr == default_credential_mock.return_value
+    on_behalf_credential_mock.assert_called_once()
+
+
+@patch("redun.azure_utils.AzureMLOnBehalfOfCredential")
+@patch("redun.azure_utils.DefaultAzureCredential")
+@patch("redun.azure_utils.is_azure_managed_node", return_value=True)
+def test_azure_default_identity_fallback_credential(
+    _, default_credential_mock, on_behalf_credential_mock
+):
+    os.environ["DEFAULT_IDENTITY_CLIENT_ID"] = "123-456-789"
     on_behalf_credential_mock.return_value.get_token = MagicMock(
         side_effect=Exception("failed to get token")
     )
     cr = azure_utils.get_az_credential()
-    del os.environ["AZUREML_WORKSPACE_ID"]
     del os.environ["DEFAULT_IDENTITY_CLIENT_ID"]
-    assert cr == mi_credential_mock.return_value
-    mi_credential_mock.assert_called_once_with(client_id="123-456-789")
+    assert cr == default_credential_mock.return_value
+    default_credential_mock.assert_called_once_with(managed_identity_client_id="123-456-789")
 
     on_behalf_credential_mock.assert_called_once()
-    default_credential_mock.assert_called_once()
 
 
-@patch("redun.azure_utils.ManagedIdentityCredential")
 @patch("redun.azure_utils.AzureMLOnBehalfOfCredential")
 @patch("redun.azure_utils.DefaultAzureCredential")
-def test_azure_fallback_credential_called_outside_aml(
-    default_credential_mock, on_behalf_credential_mock, mi_credential_mock
+@patch("redun.azure_utils.is_azure_managed_node", return_value=True)
+def test_azure_aml_job_identity_used_before_default_identity(
+    _, default_credential_mock, on_behalf_credential_mock
 ):
+    os.environ["AZUREML_USER_MANAGED_CLIENT_ID"] = "0000-0000"
     os.environ["DEFAULT_IDENTITY_CLIENT_ID"] = "123-456-789"
-    default_credential_mock.return_value.get_token = MagicMock(
-        side_effect=Exception("failed to get token")
-    )
     on_behalf_credential_mock.return_value.get_token = MagicMock(
         side_effect=Exception("failed to get token")
     )
     cr = azure_utils.get_az_credential()
     del os.environ["DEFAULT_IDENTITY_CLIENT_ID"]
-    assert cr == mi_credential_mock.return_value
-    mi_credential_mock.assert_called_once_with(client_id="123-456-789")
+    del os.environ["AZUREML_USER_MANAGED_CLIENT_ID"]
+    assert cr == default_credential_mock.return_value
+    default_credential_mock.assert_called_once_with(managed_identity_client_id="0000-0000")
 
-    on_behalf_credential_mock.assert_not_called()
-    default_credential_mock.assert_called_once()
+    on_behalf_credential_mock.assert_called_once()
 
 
-@patch("redun.azure_utils.ManagedIdentityCredential")
 @patch("redun.azure_utils.AzureMLOnBehalfOfCredential")
 @patch("redun.azure_utils.DefaultAzureCredential")
-def test_azure_fallback_credentials_not_called_with_no_env(
-    default_credential_mock, on_behalf_credential_mock, mi_credential_mock
+@patch("redun.azure_utils.is_azure_managed_node", return_value=True)
+def test_azure_client_id_variable_takes_precedence(
+    _, default_credential_mock, on_behalf_credential_mock
 ):
-    default_credential_mock.return_value.get_token = MagicMock(
-        side_effect=Exception("failed to get token")
-    )
+    os.environ["AZURE_CLIENT_ID"] = "111-111"
+    os.environ["AZUREML_USER_MANAGED_CLIENT_ID"] = "0000-0000"
+    os.environ["DEFAULT_IDENTITY_CLIENT_ID"] = "123-456-789"
     on_behalf_credential_mock.return_value.get_token = MagicMock(
         side_effect=Exception("failed to get token")
     )
+    cr = azure_utils.get_az_credential()
+    del os.environ["DEFAULT_IDENTITY_CLIENT_ID"]
+    del os.environ["AZUREML_USER_MANAGED_CLIENT_ID"]
+    del os.environ["AZURE_CLIENT_ID"]
+    assert cr == default_credential_mock.return_value
+    default_credential_mock.assert_called_once_with(managed_identity_client_id="111-111")
 
-    with pytest.raises(ValueError):
-        _ = azure_utils.get_az_credential()
-
-    default_credential_mock.assert_called_once()
-    on_behalf_credential_mock.assert_not_called()
-    mi_credential_mock.assert_not_called()
+    on_behalf_credential_mock.assert_called_once()
 
 
-@patch("redun.azure_utils.ManagedIdentityCredential")
 @patch("redun.azure_utils.AzureMLOnBehalfOfCredential")
 @patch("redun.azure_utils.DefaultAzureCredential")
-def test_azure_raise_error_for_aml_no_managed_identity(
-    default_credential_mock, on_behalf_credential_mock, mi_credential_mock
+@patch("redun.azure_utils.is_azure_managed_node", return_value=True)
+def test_azure_raise_error_if_all_credentials_fail_to_get_token(
+    _, default_credential_mock, on_behalf_credential_mock
 ):
     """
     This test checks if in case of error getting AML credential
@@ -120,13 +119,8 @@ def test_azure_raise_error_for_aml_no_managed_identity(
         side_effect=Exception("failed to get token")
     )
 
-    os.environ["AZUREML_WORKSPACE_ID"] = "test"
-
-    with pytest.raises(ValueError):
+    with pytest.raises(Exception):
         _ = azure_utils.get_az_credential()
-
-    del os.environ["AZUREML_WORKSPACE_ID"]
 
     default_credential_mock.assert_called_once()
     on_behalf_credential_mock.assert_called_once()
-    mi_credential_mock.assert_not_called()
