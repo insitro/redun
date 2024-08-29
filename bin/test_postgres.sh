c82dda2570474e7beee3caf0719bc566359362c5
Matt Rasmussen <56042231+mattrasmus@users.noreply.github.com>
2024-08-29 06:41:29 -0700

    test for docker-compose (#378)

--- bin/test_postgres.sh
+++ bin/test_postgres.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . $DIR/../.venv/bin/activate
 
 # Start database.
-docker compose up -d
+which docker-compose && docker-compose up -d || docker compose up -d
 
 # Wait for database to be ready.
 until psql postgresql://postgres:postgres@localhost:5432/postgres -c "select 1" > /dev/null 2>&1; do
@@ -16,5 +16,5 @@ REDUN_TEST_POSTGRES=1 pytest redun $TEST_ARGS
 EXIT_CODE=$?
 
 # Shutdown database.
-docker compose down
+which docker-compose && docker-compose down || docker compose down
 exit $EXIT_CODE
